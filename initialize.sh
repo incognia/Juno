496a15b7950fc2df338643b164a7ea9922f9e363
Rodrigo Álvarez <incognia@gmail.com>
2023-07-22 01:04:57 -0600

    Update initialize.sh script

--- initialize.sh
+++ initialize.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 # Crear archivos de configuración para el contenedor
-
 echo -e "version: '3.8'\nservices:" > compose.yaml
-echo -e "FROM\n\nRUN\n\nCOPY app/* /app/\n\nENTRYPOINT [ "/entrypoint.sh" ]" > Dockerfile
-
-# Crear el script de inicio
-
-echo -e "#!/bin/bash" > entrypoint.sh
-chmod +x entrypoint.sh
+echo -e "FROM image:tag\n\n" > Dockerfile
+echo -e "RUN apt-get update && apt-get install -y\n\n" >> Dockerfile
+echo -e "COPY app/* /app/\n\n" >> Dockerfile
+echo -e "COPY entrypoint.sh /entrypoint.sh\n\n" >> Dockerfile
+echo -e "RUN chmod +x /entrypoint.sh\n\n" >> Dockerfile
+echo -e "ENTRYPOINT [ "/entrypoint.sh" ]" >> Dockerfile
 
 # Crear directorio app
-
 mkdir app
+touch app/app.txt
+
+# Crear el script de inicio
+echo -e "#!/bin/bash" > entrypoint.sh
+chmod +x entrypoint.sh
