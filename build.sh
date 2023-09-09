c9f1ab0e4de78b402150e1d84c0909ca29d8f94b
Rodrigo Álvarez <incognia@gmail.com>
2023-09-08 22:00:46 -0600

    Update 09/08/23 22:00:46

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# Define una variable 'image' que almacena la imagen de Docker que se utilizará en el script.
 image=eureka/jupyterlab:0.0.1-bookworm-slim
 
+# Variables booleanas que indicarán qué acciones se deben realizar. Inicialmente, todas están configuradas como falsas.
 all=false
 build=false
 clean=false
@@ -9,6 +11,7 @@ prune=false
 rebuild=false
 volume=false
 
+# Inicio del bucle while que procesa las opciones de línea de comandos.
 while [[ $# -gt 0 ]]; do
   case "$1" in
     -a)
@@ -36,8 +39,9 @@ while [[ $# -gt 0 ]]; do
   esac
   shift
 done
+# Fin del bucle while que analiza las opciones de línea de comandos y establece las variables booleanas según corresponda.
 
-# Agregar un caso para manejar cuando no se proporcionan opciones
+# Si ninguna de las variables booleanas está configurada como verdadera, muestra un mensaje de uso y sale con un código de error.
 if [ "$all" = false ] && [ "$build" = false ] && [ "$clean" = false ] && [ "$prune" = false ] && [ "$rebuild" = false ] && [ "$volume" = false ]; then
   echo "Uso: $0 [-a] [-b] [-c] [-p] [-r] [-v]"
   echo "Opciones:"
@@ -50,34 +54,40 @@ if [ "$all" = false ] && [ "$build" = false ] && [ "$clean" = false ] && [ "$pru
   exit 1
 fi
 
+# Si la opción -a está configurada como verdadera, establece todas las demás opciones como verdaderas también.
 if [ "$all" = true ]; then
+  
   clean=true
   volume=true
   build=true
 fi
 
+# Si la opción -r está configurada como verdadera, ejecuta un mensaje específico.
 if [ "$rebuild" = true ]; then
   echo "Ejecutando código para la opción -r"
   clean=true
   build=true
 fi
 
+# Si la opción -c o -r está configurada como verdadera, ejecuta acciones para limpiar contenedores e imágenes de Docker.
 if [ "$clean" = true ]; then
   echo "Detener y eliminar los contenedores"
   sleep 2
   docker compose down
-  # Eliminar una imagen específica
+  # Eliminar una imagen específica.
   docker rmi $image
-  # Limpiar recursos no utilizados de Docker
+  # Limpiar recursos no utilizados de Docker.
   prune=true
 fi
 
+# Si la opción -p está configurada como verdadera, limpia recursos no utilizados de Docker.
 if [ "$prune" = true ]; then
   echo "Limpiar recursos no utilizados de Docker"
   sleep 2
   yes | docker system prune
 fi
 
+# Si la opción -v está configurada como verdadera, elimina volúmenes específicos de Docker.
 if [ "$volume" = true ]; then
   echo "Borrar volúmenes"
   sleep 2
@@ -92,8 +102,9 @@ if [ "$volume" = true ]; then
     jupyter_arkhimedes_ssh
 fi
 
+# Si la opción -b o -a está configurada como verdadera, inicia los contenedores en modo desacoplado.
 if [ "$build" = true ]; then
-  echo "Iniciar los contenedores en modo desatachado"
+  echo "Iniciar los contenedores en modo desacoplado"
   sleep 2
   docker compose up -d
 fi
