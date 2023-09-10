5035880c903411c0d7bb7160978ea2118788d9d6
Rodrigo Álvarez <incognia@gmail.com>
2023-09-09 19:03:53 -0600

    Update 09/09/23 19:03:53

--- build.sh
+++ build.sh
@@ -13,98 +13,97 @@ volume=false
 
 # Inicio del bucle while que procesa las opciones de línea de comandos.
 while [[ $# -gt 0 ]]; do
-  case "$1" in
+    case "$1" in
     -a)
-      all=true
-      ;;
+        all=true
+        ;;
     -b)
-      build=true
-      ;;
+        build=true
+        ;;
     -c)
-      clean=true
-      ;;
+        clean=true
+        ;;
     -p)
-      prune=true
-      ;;
+        prune=true
+        ;;
     -r)
-      rebuild=true
-      ;;
+        rebuild=true
+        ;;
     -v)
-      volume=true
-      ;;
+        volume=true
+        ;;
     *)
-      echo "Opción no válida: $1"
-      exit 1
-      ;;
-  esac
-  shift
+        echo "Opción no válida: $1"
+        exit 1
+        ;;
+    esac
+    shift
 done
 # Fin del bucle while que analiza las opciones de línea de comandos y establece las variables booleanas según corresponda.
 
 # Si ninguna de las variables booleanas está configurada como verdadera, muestra un mensaje de uso y sale con un código de error.
 if [ "$all" = false ] && [ "$build" = false ] && [ "$clean" = false ] && [ "$prune" = false ] && [ "$rebuild" = false ] && [ "$volume" = false ]; then
-  echo "Uso: $0 [-a] [-b] [-c] [-p] [-r] [-v]"
-  echo "Opciones:"
-  echo "  -a  Realizar todas las acciones (limpiar, construir, limpiar volúmenes)"
-  echo "  -b  Construir los contenedores"
-  echo "  -c  Limpiar contenedores y eliminar la imagen"
-  echo "  -p  Limpiar recursos no utilizados de Docker"
-  echo "  -r  Reconstruir los contenedores (implica limpiar y construir)"
-  echo "  -v  Borrar volúmenes"
-  exit 1
+    echo "Uso: $0 [-a] [-b] [-c] [-p] [-r] [-v]"
+    echo "Opciones:"
+    echo "  -a  Realizar todas las acciones (limpiar, construir, limpiar volúmenes)"
+    echo "  -b  Construir los contenedores"
+    echo "  -c  Limpiar contenedores y eliminar la imagen"
+    echo "  -p  Limpiar recursos no utilizados de Docker"
+    echo "  -r  Reconstruir los contenedores (implica limpiar y construir)"
+    echo "  -v  Borrar volúmenes"
+    exit 1
 fi
 
 # Si la opción -a está configurada como verdadera, establece todas las demás opciones como verdaderas también.
 if [ "$all" = true ]; then
-  
-  clean=true
-  volume=true
-  build=true
+    clean=true
+    volume=true
+    build=true
 fi
 
 # Si la opción -r está configurada como verdadera, ejecuta un mensaje específico.
 if [ "$rebuild" = true ]; then
-  echo "Ejecutando código para la opción -r"
-  clean=true
-  build=true
+    echo "Ejecutando código para la opción -r"
+    clean=true
+    build=true
 fi
 
 # Si la opción -c o -r está configurada como verdadera, ejecuta acciones para limpiar contenedores e imágenes de Docker.
 if [ "$clean" = true ]; then
-  echo "Detener y eliminar los contenedores"
-  sleep 2
-  docker compose down
-  # Eliminar una imagen específica.
-  docker rmi $image
-  # Limpiar recursos no utilizados de Docker.
-  prune=true
+    echo "Detener y eliminar los contenedores"
+    sleep 2
+    docker compose down
+    # Eliminar una imagen específica.
+    docker rmi $image
+    # Limpiar recursos no utilizados de Docker.
+    prune=true
 fi
 
 # Si la opción -p está configurada como verdadera, limpia recursos no utilizados de Docker.
 if [ "$prune" = true ]; then
-  echo "Limpiar recursos no utilizados de Docker"
-  sleep 2
-  yes | docker system prune
+    echo "Limpiar recursos no utilizados de Docker"
+    sleep 2
+    yes | docker system prune
 fi
 
 # Si la opción -v está configurada como verdadera, elimina volúmenes específicos de Docker.
 if [ "$volume" = true ]; then
-  echo "Borrar volúmenes"
-  sleep 2
-  docker volume rm \
-    jupyter_newton_home \
-    jupyter_newton_ssh \
-    jupyter_darwin_home \
-    jupyter_darwin_ssh \
-    jupyter_euler_home \
-    jupyter_euler_ssh \
-    jupyter_arkhimedes_home \
-    jupyter_arkhimedes_ssh
+    echo "Borrar volúmenes"
+    sleep 2
+    docker volume rm \
+        jupyter_newton_home \
+        jupyter_newton_ssh \
+        jupyter_darwin_home \
+        jupyter_darwin_ssh \
+        jupyter_euler_home \
+        jupyter_euler_ssh \
+        jupyter_arkhimedes_home \
+        jupyter_arkhimedes_ssh
 fi
 
 # Si la opción -b o -a está configurada como verdadera, inicia los contenedores en modo desacoplado.
 if [ "$build" = true ]; then
-  echo "Iniciar los contenedores en modo desacoplado"
-  sleep 2
-  docker compose up -d
+    echo "Iniciar los contenedores en modo desacoplado"
+    sleep 2
+    docker compose up -d
 fi
