44bfa465e96e097fbdcf3bc5dde2a4307f426d26
Rodrigo Álvarez <incognia@gmail.com>
2023-09-11 23:34:09 -0600

    Add color to Python scripts

--- app/notes/notes.py
+++ app/notes/notes.py
@@ -2,14 +2,23 @@
 
 import os
 
+# Define los códigos de color ANSI
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE="\033[34m"
+ENDCOLOR = "\033[0m"
+
 # Mostrar una lista de directorios disponibles para copia
-print("Directorios disponibles para copia:")
+print(f"\n{BLUE}Directorios disponibles para copia:{ENDCOLOR}\n")
 for entry in os.scandir('.'):
     if entry.is_dir():
         print(entry.name)
 
 # Solicitar al usuario el nombre de la subcarpeta a copiar
-subcarpeta = input("Ingrese el nombre de la subcarpeta a copiar: ")
+subcarpeta = input(f"\n{YELLOW}Ingrese el nombre de la subcarpeta a copiar: {ENDCOLOR}")
+
+print()
 
 # Leer la lista de nombres de contenedores desde el archivo "containers.txt"
 with open("containers.txt", "r") as file:
@@ -23,4 +32,4 @@ for container_name in container_names:
 for container_name in container_names:
     os.system(f'docker exec -it {container_name} chown -R eureka:eureka "/home/eureka/{subcarpeta}"')
 
-print("Proceso completado.")
+print(f"\n{GREEN}Proceso completado{ENDCOLOR}\n")
--- build.py
+++ build.py
@@ -8,6 +8,7 @@ import time
 RED = "\033[31m"
 GREEN = "\033[32m"
 YELLOW = "\033[33m"
+BLUE="\033[34m"
 ENDCOLOR = "\033[0m"
 
 # Define la imagen de Docker
