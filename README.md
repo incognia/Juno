ef37471179ffa64da7e5cd4c2c25f0985ee32a9d
Rodrigo Álvarez <incognia@gmail.com>
2023-09-17 19:30:44 -0600

    Udate README.md

--- README.md
+++ README.md
@@ -336,20 +336,30 @@ Juno/
 │       ├── 01_basics/
 │       │   ├── 01_string_input_print.ipynb
 │       │   └── 02_variables.ipynb
+│       ├── 02_if_else/
+│       │   ├── 03_list_if_in_else.ipynb
+│       │   ├── 04_list_append_remove.ipynb
+│       │   ├── 05_list_index_pop_insert.ipynb
+│       │   ├── 06_list_slicing.ipynb
+│       │   └── 07_list_slicing_use.ipynb
 │       ├── [...]
 │       │   └── [...]
 │       ├── 06_recap/
 │       │   └── 21_list_recap.ipynb
 │       ├── containers.txt -> ../../containers.txt
+│       ├── eureka.txt
+│       ├── logos.txt
 │       └── notes.py*
-├── build.py*
 ├── build.sh*
+├── CODE_OF_CONDUCT.md
 ├── compose.yaml
 ├── containers.txt
+├── CONTRIBUTING.md
 ├── Dockerfile
 ├── entrypoint.sh*
 ├── generator.py*
 ├── LICENSE
+├── ports.py*
 └── README.md
 ```
 
@@ -362,6 +372,6 @@ I would like to extend my sincere thanks and credit to [Serena Bonaretti](https:
 
 Serena's dedication to making Python accessible through Jupyter notebooks has been a tremendous influence on this project, and I'm grateful for her contributions to the Python community.
 
-I would also like to express my heartfelt gratitude to three of my outstanding students, Carlos, [Ian](https://github.com/calmestprism226), and Fernando. Their remarkable talent, curiosity, and intelligence were instrumental in the development of the solution documented in this text. Teaching them for over 6 years has not only made me a better instructor but also a better human being. Their support and feedback have been invaluable throughout this journey.
+I would also like to express my heartfelt gratitude to three of my outstanding students, [Carlos](https://github.com/cgzdev), [Ian](https://github.com/calmestprism226), and Fernando. Their remarkable talent, curiosity, and intelligence were instrumental in the development of the solution documented in this text. Teaching them for over 6 years has not only made me a better instructor but also a better human being. Their support and feedback have been invaluable throughout this journey.
 
 Special thanks go to my dear friend and apprentice, [Amelia Chavelas](https://github.com/ameliadharani). Her unwavering support, patience, enthusiasm, and encouragement mean the world to me, not only in this project but in everything I do.
--- app/notes/notes.py
+++ app/notes/notes.py
@@ -10,18 +10,56 @@ YELLOW = "\033[33m"
 BLUE = "\033[34m"
 ENDCOLOR = "\033[0m"
 
+# Función para mostrar el mensaje de ayuda
+def mostrar_ayuda():
+    print(f"""Uso: {sys.argv[0]} [-a | -e | -l | -h]
+
+Opciones:
+  -a              Copiar todas las subcarpetas a los contenedores.
+  -e              Usar el archivo "eureka.txt" para los nombres de contenedores.
+  -l              Usar el archivo "logos.txt" para los nombres de contenedores.
+  -h              Mostrar este mensaje de ayuda.
+
+Ejemplos de uso:
+  1. Copiar todas las subcarpetas a los contenedores usando el archivo predeterminado "containers.txt":
+     {sys.argv[0]} -a
+
+  2. Copiar una subcarpeta específica a los contenedores usando el archivo "eureka.txt":
+     {sys.argv[0]} -e
+
+  3. Copiar una subcarpeta específica a los contenedores usando el archivo "logos.txt":
+     {sys.argv[0]} -l
+
+  4. Mostrar este mensaje de ayuda:
+     {sys.argv[0]} -h
+""")
+
+# Verificar si se proporciona el argumento -h
+if '-h' in sys.argv:
+    mostrar_ayuda()
+    sys.exit()
+
 # Mostrar una lista de directorios disponibles para copia
 print(f"\n{BLUE}Directorios disponibles para copia:{ENDCOLOR}\n")
 for entry in os.scandir('.'):
     if entry.is_dir():
         print(entry.name)
 
+# Definir nombres de archivo predeterminados
+container_file = "containers.txt"
+
+# Verificar opciones de línea de comandos
+if '-e' in sys.argv:
+    container_file = "eureka.txt"
+elif '-l' in sys.argv:
+    container_file = "logos.txt"
+
+# Leer la lista de nombres de contenedores desde el archivo
+with open(container_file, "r") as file:
+    container_names = [line.strip().capitalize() for line in file]
+
 # Verificar si se proporciona el argumento -a
 if '-a' in sys.argv:
-    # Leer la lista de nombres de contenedores desde el archivo "containers.txt"
-    with open("containers.txt", "r") as file:
-        container_names = [line.strip().capitalize() for line in file]
-
     # Copiar todas las subcarpetas a los contenedores
     for subcarpeta in os.listdir('.'):
         if os.path.isdir(subcarpeta):
@@ -40,10 +78,6 @@ else:
     subcarpeta = input(f"\n{YELLOW}Ingrese el nombre de la subcarpeta a copiar: {ENDCOLOR}")
 
     print()
-
-    # Leer la lista de nombres de contenedores desde el archivo "containers.txt"
-    with open("containers.txt", "r") as file:
-        container_names = [line.strip().capitalize() for line in file]
 
     # Copiar la subcarpeta a los contenedores
     for container_name in container_names:
